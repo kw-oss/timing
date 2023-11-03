61a515bb706d18cb3ed9a6f53bad0956a2a362ff
Doldamul <junha0504@gmail.com>
2023-11-03 23:08:37 +0900

    [feat] 선호 음식 질문 UI 표 형태로 변경.

--- main.py
+++ main.py
@@ -1,6 +1,70 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
+from typing import List, Dict
+
+class SurveySet(ttk.Frame):
+    # creates a questionaire in tabular form, with given score names and subtitles
+    def __init__(self, container, score_names: List[int], subtitles: List[str], *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
+        # instance members
+        self.score_labels: [ttk.Label] = []
+        self.radio_sets: List[RadioSet] = []
+        self.answers: Dict[str, IntVar] = {}
+
+        # set default value of answers, and initialize self.answers
+        default_value = (len(score_names) - 1) // 2
+        for subtitle in subtitles:
+            self.answers[subtitle] = IntVar(self, value=default_value)
+
+        # set score_label row
+        for i, score_name in enumerate(score_names):
+            # workaround for radio button's misalignment to center
+            score_name += " "
+
+            new_label = ttk.Label(self, text=score_name)
+            new_label.grid(column=i+1, row=0)
+            
+            self.score_labels.append(new_label)
+
+        # set radio_set rows
+        for i, subtitle in enumerate(subtitles):
+            new_radioset = RadioSet(self, pos=(0, i+1), title=subtitle, radio_count=len(score_names), variable=self.answers[subtitle])
+
+            self.radio_sets.append(new_radioset)
+
+        # set column weight
+        self.columnconfigure(0, weight=0)
+        for i in range(len(score_names)):
+            # uniform: share common width between columns that has same key str
+            self.columnconfigure(i+1, weight=2, uniform="radio set")
+
+        # set row weight
+        for i in range(len(subtitles)):
+            self.rowconfigure(i+1, weight=2)
+
+class RadioSet:
+    # create and attach a label and number of radio button to container, start at given position
+    def __init__(self, container, pos: (int, int), title: str, radio_count: int, variable: IntVar, *args, **kwargs):
+        # instance members
+        self.radio: List[ttk.Radiobutton] = []
+        self.label: ttk.Label
+        self.variable = variable
+        
+        (col, row) = pos
+        
+        # set title label
+        self.label = ttk.Label(container, text=title)
+        self.label.grid(column=col, row=row, sticky=E, padx=(0, 10))
+
+        # set radio buttons
+        for i in range(radio_count):
+            col += 1
+        
+            new_radio = ttk.Radiobutton(container, text="", value=i, variable=self.variable)
+            new_radio.grid(column=col, row=row)
+            
+            self.radio.append(new_radio)
 
 # TODO: scrolling via mouse wheel
 # TODO: auto hiding scrollbar when not needed
@@ -61,18 +125,19 @@ class ListItem(ttk.Frame):
         self.distance_label  .grid(column=0, row=1, columnspan=2, sticky=W)
         self.time_label      .grid(column=0, row=2, columnspan=2, sticky=W)
         self.rating_label    .grid(column=1, row=2, sticky=W)
-        
+
 def mainButtonPressed():
     # put some search result to `data`
     data = None
     displaySearchResult(data)
-    print(preference)
+    for key, value in survey.answers.items():
+        print(key, value.get(), sep=": ")
 
 
 def displaySearchResult(data):
     # hide question label
-    questionLabel.grid_remove()
-    foodCategoryCheckButtons.grid_remove()
+    question_label.grid_remove()
+    survey.grid_remove()
 
     # prevents displaying large window bigger than minsize
     width = map_placeholder.width()
@@ -85,7 +150,9 @@ def displaySearchResult(data):
     map.grid()
     listframe.grid()
     mainframe.rowconfigure(1, weight=1)
+    
     buttomBar.grid()
+    mainframe.rowconfigure(2, weight=0)
     
     # change some layout configuration of runButton
     runButton.grid(sticky=S)
@@ -95,33 +162,6 @@ def displaySearchResult(data):
     (width, height) = currentWindowSize()
     window.minsize(width, height)
     window.maxsize(int(width * 1.5), height * 2)
-
-
-def checkButtonList(master, names: set) -> ttk.Frame:
-    frame = ttk.Frame(master)
-
-    i = 0
-    for name in names:
-        name = str(name)
-        check = ttk.Checkbutton(frame, text=name)
-        
-        # 'lambda n=name:' 구문: 현재 name 값을 n값으로 '즉시' 캡쳐
-        # 즉시 캡쳐하지 않을 경우 lambda 표현식은 해당 변수를 '호출 시점'에 참조하므로 의도가 달라짐.
-        check.configure(command=lambda n=name: listPushOrPop(n))
-        check.state(['!alternate'])
-        check.grid(column=0, row=i, sticky=W, pady=1)
-        i += 1
-
-
-    def listPushOrPop(name: str):
-        if name in names:
-            names.remove(name)
-        else:
-            names.add(name)
-
-
-    names.clear()
-    return frame
 
 
 def currentWindowSize() -> (int, int):
@@ -150,16 +190,9 @@ if __name__ == '__main__':
     map.grid(column=0, row=0, sticky=N)
     mainframe.rowconfigure(0, weight=0)
 
-    questionLabel = ttk.Label(mainframe)
-    questionLabel.configure(text="지금 어떤 종류의 음식을 원하시나요?")
-    questionLabel.grid(column=0, row=0, sticky=[W, N])
-
-    preference = {"중식", "일식", "한식", "간식", "휴식"}
-    foodCategoryCheckButtons = checkButtonList(mainframe, preference)
-    foodCategoryCheckButtons.grid(column=0, row=1, sticky=[W, N], pady=(10, 30))
-
     listframe = ScrollableFrame(mainframe)
     listframe.grid(column=0, row=1, sticky=(N, W, E, S))
+    mainframe.rowconfigure(1, weight=1)
 
     # give some placeholder item to the list
     for i in range(10):
@@ -169,18 +202,28 @@ if __name__ == '__main__':
 
     buttomBar = ttk.Separator(mainframe, orient='horizontal')
     buttomBar.grid(column=0, row=2, sticky=[E, W])
-    mainframe.rowconfigure(2, weight=0)
+    mainframe.rowconfigure(2, weight=4)
 
     map.grid_remove()
     listframe.grid_remove()
     buttomBar.grid_remove()
+
+    question_text = "각 음식 종류별 선호도를 알려주세요."
+    question_label = ttk.Label(mainframe)
+    question_label.configure(text=question_text)
+    question_label.grid(column=0, row=0, sticky=[W, N])
+
+    score_kinds = ["싫어함", "별로", "그럭저럭", "좋아함", "땡김"]
+    food_kinds = ["중식", "일식", "한식", "디저트"]
+    survey = SurveySet(mainframe, score_names=score_kinds, subtitles=food_kinds)
+    survey.grid(column=0, row=1, sticky=(N, W, E, S), pady=(10, 30))
 
     runButton = ttk.Button(mainframe)
     runButton.config(text="Run")
     runButton.config(command=mainButtonPressed)
     
     runButton.grid(column=0, row=3, sticky=[N, E, S, W])
-    mainframe.rowconfigure(3, weight=1)
+    mainframe.rowconfigure(3, weight=2)
 
     # set the window's size to fit the initial content
     (width, height) = currentWindowSize()
