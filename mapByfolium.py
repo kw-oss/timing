7d515bd9589bd03bc3c403e841b9db3e35a5b73a
kijan <wogks03@naver.com>
2023-11-04 03:06:18 +0900

    마커 출력

--- mapByfolium.py
+++ mapByfolium.py
@@ -6,10 +6,19 @@ lat = 37.6194319
 #경도
 lon = 127.0572451
 
-m = folium.Map(location=[lat, lon],
-               zoom_start=17,
-               width= 750,
-               height=500
-            )
+def makeMap():
+    m = folium.Map(location=[lat, lon],
+                zoom_start=17,
+                width= 1024,
+                height=1000
+                )
 
-m.save('map.html')
+    folium.Marker(
+                [37.62006077967472, 127.05818653255353],
+                popup ="설빙",
+                #tooltip= "설빙2층"
+                ).add_to(m)
+
+    m.save('map.html')
+    
+makeMap()
