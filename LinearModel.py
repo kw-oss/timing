627ab20a2a594307a661671430e9a0c0a005546c
Yoonkyo Hong <114062987+Hyykk@users.noreply.github.com>
2023-11-07 20:30:35 +0900

    [perf], [feat] 모델 성능개선
    모델 성능 개선과 예측한 추천율에 따라서 정렬하는 코드 추가

--- LinearModel.py
+++ LinearModel.py
@@ -14,6 +14,7 @@ from os.path import join
 
 class RecommendationModel:
     def __init__(self):
+        self.scaler = StandardScaler()
         self.model = self.build_model()
 
     def build_model(self):
@@ -27,9 +28,9 @@ class RecommendationModel:
 
     def train(self, X, y, epochs=100, batch_size=32, test_size=0.2, random_state=42):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-        scaler = StandardScaler()
-        X_train_scaled = scaler.fit_transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
+        self.scaler.fit(X_train)
+        X_train_scaled = self.scaler.fit_transform(X_train)
+        X_test_scaled = self.scaler.transform(X_test)
         self.model.fit(X_train_scaled, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test_scaled, y_test))
 
     def evaluate(self, X, y):
@@ -38,7 +39,7 @@ class RecommendationModel:
         return loss, mae
 
     def predict(self, X):
-        X_scaled = StandardScaler().transform(X)
+        X_scaled = self.scaler.transform(X)
         predictions = self.model.predict(X_scaled)
         return predictions
 
@@ -82,3 +83,15 @@ if __name__ == "__main__":
     # 모델 생성
     model = RecommendationModel()
     model.train(X, y)
+
+    # 여기 X에는 실제 데이터가 들어가야합니다! (지금은 잘 나오나 원래 데이터로 돌려봤어요.)
+    Predict_data = model.predict(X)
+
+    # 실제 데이터프레임(placesDF 말고)에서 '추천율' coliumn을 생성한 후에 예측한 추천율을 넣어놓습니다.
+    placesDF['추천율'] = 0
+    placesDF['추천율'] = Predict_data
+
+    # 추천율을 기준으로 정렬합니다. (추천율이 높은 순서대로 하기위해서 ascending = False를 사용했습니다.)
+    placesDF.sort_values('추천율', ascending = False)
+
+    print(placesDF)
